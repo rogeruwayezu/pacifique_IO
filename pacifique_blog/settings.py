9bdeb08daf413b126d67852593067243c3492914
Pacifique <rogeruwayezu@gmail.com>
2020-03-08 09:38:50 +0200

    finished the homepage

--- pacifique_blog/settings.py
+++ pacifique_blog/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'pacifique.apps.PacifiqueConfig',
+    'pacifique',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
